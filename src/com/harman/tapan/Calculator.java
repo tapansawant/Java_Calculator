f133955788a6ef781b9911a9ddbbf1600d80ebdf
tapansawant <tapansawant45@gmail.com>
2022-03-15 15:50:53 +0530

    Created Addition function

--- src/com/harman/tapan/Calculator.java
+++ src/com/harman/tapan/Calculator.java
@@ -3,7 +3,15 @@ package com.harman.tapan;
 import java.util.Scanner;
 
 public class Calculator {
+    public int AddOperation(int x, int y)
+    {
+        int z = x + y;
+        return z;
+    }
+
+
     public static void main(String[] args) {
+        Calculator calc = new Calculator(); //Creating Object
 
         int n1,n2,result;
         Scanner input = new Scanner(System.in);
@@ -13,7 +21,7 @@ public class Calculator {
         System.out.println("Enter num2: ");
         n2 = input.nextInt();
 
-        result = n1 + n2;
+        result = calc.AddOperation(n1, n2);
         System.out.println(result);
     }
 }
